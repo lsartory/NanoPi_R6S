73eaea9248fce6edafbaea27de4b29c7e8a61764
Lambert Sartory <lambert.sartory@gmail.com>
2023-01-01 19:54:47 +0100

    Prevent the screen from turning off when using Moonlight

--- usr/bin/moonlight-rk.sh
+++ usr/bin/moonlight-rk.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-xautolock -disable
+
+xautolock -exit &> /dev/null
+xset s off -dpms
 xrandr --output HDMI-1 --mode 1920x1080 --rate 60
+
 moonlight-embedded stream -app Desktop -platform rk -1080 -bitrate 80000 "$1" > /dev/null
+
 xrandr --output HDMI-1 --mode 3840x2160 --rate 60
-xautolock -enable
+xset s on +dpms
+xautolock -time 5 -locker slock &
